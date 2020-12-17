2588bad139312887bfac8c112a46fc748c93d23d
libingnan <libingnan@xiaobangtouzi.com>
2020-12-17 19:54:20 +0800

    init

---
+++ src/main/java/leetcode/editor/cn/RemoveDuplicatesFromSortedListIi.java
@@ -0,0 +1,52 @@
+//给定一个排序链表，删除所有含有重复数字的节点，只保留原始链表中 没有重复出现 的数字。 
+//
+// 示例 1: 
+//
+// 输入: 1->2->3->3->4->4->5
+//输出: 1->2->5
+// 
+//
+// 示例 2: 
+//
+// 输入: 1->1->1->2->3
+//输出: 2->3 
+// Related Topics 链表 
+// 👍 412 👎 0
+
+package leetcode.editor.cn;
+
+public class RemoveDuplicatesFromSortedListIi {
+    public static void main(String[] args) {
+        Solution solution = new RemoveDuplicatesFromSortedListIi().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public ListNode deleteDuplicates(ListNode head) {
+            ListNode ln = new ListNode();
+            ListNode pn = head;
+            while (pn != null) {
+                if (ln.val != pn.val) {
+                    ln = pn;
+                    pn = pn.next;
+                } else {
+                    
+                }
+
+            }
+
+
+            return head;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
